c9daf3b48caffec44525d295bff8d97c7484ac8b
Eric Moritz <eric.moritz@gmail.com>
2019-05-30 17:46:19 -0400

    fonts and stuff

--- doom.d/config.el
+++ doom.d/config.el
@@ -2,5 +2,5 @@
 
 ;; Place your private configuration here
 (display-time-mode 1)
-(setq doom-theme 'doom-Iosvkem)
-(setq doom-font (font-spec :family \"Fira Mono\" :size 12))
+(setq doom-theme 'doom-tomorrow-night)
+(setq doom-font (font-spec :family "Source Code Pro" :size 16))
