dcc22e4bf5d078b6219197a659334c6bb76e083c
Eric Moritz <eric@corsha.com>
2019-06-20 10:04:40 -0400

    stuff

--- doom.d/config.el
+++ doom.d/config.el
@@ -5,3 +5,12 @@
 (setq doom-theme 'doom-tomorrow-night)
 (setq doom-font (font-spec :family "Source Code Pro" :size 14))
 (set-formatter! 'yq "cat")
+(setq prettier-js-args '(
+  "--trailing-comma" "all"
+  "single-quote" "true"
+))
+(setq
+ doom-modeline-major-mode-icon t
+ doom-modeline-persp-name t
+ org-clock-clocked-in-display 'both
+ )
