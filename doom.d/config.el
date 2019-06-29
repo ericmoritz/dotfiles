3c3513e8a18966ce611e4c49829e0b2349fc570e
Eric Moritz <eric@corsha.com>
2019-06-29 10:58:00 -0400

    Added the monoid font to emacs

--- doom.d/config.el
+++ doom.d/config.el
@@ -3,7 +3,7 @@
 ;; Place your private configuration here
 (display-time-mode 1)
 (setq doom-theme 'doom-dracula)
-(setq doom-font (font-spec :family "Source Code Pro" :size 14))
+(setq doom-font (font-spec :family "Monoid" :size 12))
 (set-formatter! 'yq "cat")
 (setq prettier-js-args '(
   "--trailing-comma" "all"
