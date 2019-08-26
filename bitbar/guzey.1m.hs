d2e19cf46539c96a06ce6eeba965981ecbc28874
Eric Moritz <eric@corsha.com>
2019-08-26 13:31:10 -0400

    feat: added a guzey clock
    Inspired by https://guzey.com/productivity/ I created a bitbar script
    to display the phase. It is more an excuse to write some Haskell than
    anything else.

---
+++ bitbar/guzey.1m.hs
@@ -0,0 +1,39 @@
+#!/usr/local/bin/stack
+{- stack
+    --resolver lts-14.2
+    script
+    --package time
+ -}
+{-# LANGUAGE OverloadedStrings #-}
+import Data.Time.Clock
+import Data.Time.LocalTime
+
+
+data Mode = LongBreak | Break | Work
+modeStr :: Mode -> String
+modeStr LongBreak = "Long Break"
+modeStr Break = "Break"
+modeStr Work = "Work"
+
+-- work for 25 minutes from :05 to :30
+-- take a 5 minute break from :30 to :35
+-- work for 25 minutes from :35 to :00
+-- take a 5 minute break from :00 to :05
+-- every three hours (at 12-3-6-9) the :05-:30 work cycle is substituted for a break, which lasts 35 minutes.
+timeToMode :: LocalTime -> Mode
+timeToMode x
+  | hour `mod` 3 == 0 && minute < 35 = LongBreak
+  | minute < 5         = Break
+  | otherwise          = Work
+  where local =localTimeOfDay x
+        hour = todHour local
+        minute = todMin local
+
+
+main :: IO ()
+main = do
+  localTime <- utcToLocalTime <$> getCurrentTimeZone <*> getCurrentTime
+  let mode = timeToMode localTime
+  putStrLn $ modeStr mode
+    
+
