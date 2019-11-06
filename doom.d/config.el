d37db01f76fd383d3b2928952685710e0a9b62e8
Eric Moritz <eric@corsha.com>
2019-11-06 11:15:32 -0500

    syncing for bug report

--- doom.d/config.el
+++ doom.d/config.el
@@ -4,18 +4,13 @@
 (load-file "~/local.el")
 
 (display-time-mode 1)
-(setq doom-theme 'doom-tomorrow-night)
-(setq doom-font (font-spec :family "Dank Mono" :size 12))
+;; (setq doom-theme 'doom-tomorrow-night)
+(setq doom-font (font-spec :family "Dank Mono" :size 14))
 
-;; yq steals my YAML comments
-(set-formatter! 'yq "cat")
-
-(setq typescript-indent-level 2)
 
 (setq
  doom-modeline-major-mode-icon t
  doom-modeline-persp-name t
- org-clock-clocked-in-display 'both
  )
 
 (setq projectile-project-search-path eam/projectile-project-search-path)
@@ -25,8 +20,6 @@
 ;; Use typescript-mode for tsx files because web-mode isn't good for typescript
 (add-to-list 'auto-mode-alist '("\\.tsx$" . typescript-mode))
 
-(add-hook! 'org-mode-hook
-  (setq org-agenda-span 14))
 
 ;; Trigger completion immediately.
 (setq company-idle-delay 0)
@@ -36,14 +29,12 @@
 
 (setq godoc-and-godef-command "go doc")
 
-(after! 'eshell
-    (push "watch" eshell-visual-commands)
-)
+;; (setq eshell-visual-commands (push "watch" eshell-visual-commands))
+
 (after! 'em-term
   (add-to-list 'eshell-visual-commands "watch"))
 
-(after! typescript-mode
-  (set-formatter! 'prettier "cat"))
+
 (bind-key "C-M-<tab>" 'company-other-backend)
 
 (after! go-mode
@@ -74,24 +65,75 @@
 (load-file "~/.doom.d/commitit.el")
 
 
-(def-package! lsp-mode
-  :hook
-  (haskell-mode . lsp)
-  (python-mode . lsp)
-  (rustic-mode . lsp)
-  (reason-mode . lsp)
-  :config
-  (lsp-register-client
-   (make-lsp-client :new-connection (lsp-stdio-connection "reason-language-server")
-                    :major-modes '(reason-mode)
-                    :notification-handlers (ht ("client/registerCapability" 'ignore))
-                    :priority 1
-                    :server-id 'reason-ls))
-  :commands
-  lsp)
+;; Add the persp name to the titlebar
+(setq
+ frame-title-format '((:eval (let
+                                 ((name (safe-persp-name (get-current-persp))))
+                                 (if name (format "#%s — " name))
+                              )) "%b — Doom Emacs"))
+;;; ~/dotfiles/doom.d/+org.el -*- lexical-binding: t; -*-
+;; Org mode stuff
+
+(setq
+ org-clock-clocked-in-display 'both
+ org-agenda-span 14
+ org-agenda-custom-commands '(
+                              ;; Inbox
+                              ("i" "Inbox"
+                               (
+                                (tags-todo "inbox+SCHEDULED=\"\"" )
+                                ))
+ )
+)
+
+;;----------------------------------------------------------------------------
+;; Reason setup
+;;----------------------------------------------------------------------------
+(defun shell-cmd (cmd)
+  "Returns the stdout output of a shell command or nil if the command returned
+   an error"
+  (car (ignore-errors (apply 'process-lines (split-string cmd)))))
+
+(defun reason-cmd-where (cmd)
+  (let ((where (shell-cmd cmd)))
+    (if (not (string-equal "unknown flag ----where" where))
+      where)))
+
+(let* ((refmt-bin (or (reason-cmd-where "refmt ----where")
+                      (shell-cmd "which refmt")
+                      (shell-cmd "which bsrefmt")))
+       (merlin-bin (or (reason-cmd-where "ocamlmerlin ----where")
+                       (shell-cmd "which ocamlmerlin")))
+       (merlin-base-dir (when merlin-bin
+                          (replace-regexp-in-string "bin/ocamlmerlin$" "" merlin-bin))))
+  ;; Add merlin.el to the emacs load path and tell emacs where to find ocamlmerlin
+  (when merlin-bin
+    (add-to-list 'load-path (concat merlin-base-dir "share/emacs/site-lisp/"))
+    (setq merlin-command merlin-bin))
+
+  (when refmt-bin
+    (setq refmt-command refmt-bin)))
+
+(require 'reason-mode)
+(require 'merlin)
+(add-hook 'reason-mode-hook (lambda ()
+                              (add-hook 'before-save-hook 'refmt-before-save)
+                              (merlin-mode)))
+
+(setq merlin-ac-setup t)
+(defun reason-mode-indent-line () t)
+
+(defun esfmt ()
+  "Format using eslint --fix"
+  (interactive)
+  (call-process-region (point-min) (point-max) "~/dotfiles/ansible/roles/react/files/esfmt" t t nil buffer-file-name)
+  )
 
 
-(def-package! reason-mode
-  :mode "\\.re$"
-  :hook
-  (before-save . refmt))
+(after! typescript-mode
+  (setq typescript-indent-level 2)
+  (set-formatter! 'prettier "cat")
+  (add-hook 'before-save-hook (lambda ()
+                                (message "running esfmt on %s" major-mode)
+                                (when (eq major-mode 'typescript-mode) (esfmt))))
+)
