a2e261a66387e9181015167f224b54e8c8f7e27e
Eric Moritz <eric@corsha.com>
2020-01-02 12:54:58 -0500

    feat: added an EOB bitbar

---
+++ bitbar/0001-eob.1s.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+import time
+from datetime import datetime
+
+EOB = "18:00"
+
+now = datetime.now()
+eob_time = datetime.strptime("{} {}".format(now.date().strftime("%m-%d-%y"), EOB), "%m-%d-%y %H:%M")
+
+diff = eob_time - now
+diff = ":".join(str(diff).split(":")[:2])
+print(diff + "h till EOB")
