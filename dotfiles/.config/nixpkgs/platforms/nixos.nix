f1f05994bbc27283efd18bdf92eb7ddcd3a644a5
Eric Moritz <eric.moritz@gmail.com>
2021-03-25 19:14:29 -0400

    add teams.desktop to mimeapps

--- dotfiles/.config/nixpkgs/platforms/nixos.nix
+++ dotfiles/.config/nixpkgs/platforms/nixos.nix
@@ -56,6 +56,7 @@ in {
         "x-scheme-handler/https" = [ "firefox.desktop" ];
         "x-scheme-handler/about" = [ "firefox.desktop" ];
         "x-scheme-handler/unknown" = [ "firefox.desktop" ];
+        "x-scheme-handler/msteams" = [ "teams.desktop" ];
       };
     };
 
